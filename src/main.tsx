4aecc8d4c82318ef51cdceec5512089d5fd4bce9
Muzamil Khan <113015136+Zaibten@users.noreply.github.com>
2025-05-29 19:25:00 +0500

    configure light theme in mobile
    configure light theme in mobile

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider storageKey='vite-ui-theme'>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
       <Toaster />
     </ThemeProvider>
